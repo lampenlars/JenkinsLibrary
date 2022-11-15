edb1498fe087d8fd4fee4802222f1b611c1b3a47
lampenlars <79268084+lampenlars@users.noreply.github.com>
2022-11-15 14:27:56 +0100

    Update MavenBuild.groovy

--- src/com/jenkins/MavenBuild.groovy
+++ src/com/jenkins/MavenBuild.groovy
@@ -5,7 +5,7 @@ import hudson.*;
 
 public class MavenBuild {
     static void cleanInstall(def steps) {
-        steps.withMaven(maven: '3.8.6') {
+        steps.withMaven(maven: 'Maven 3.8.6') {
             steps.echo "CallCounter: " + CallCounter.count()
             steps.sh 'mvn clean install'
         }
